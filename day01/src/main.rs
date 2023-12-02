4f04865a0422875e846964e155d0c643b1e6a4f2
Mihails Strasuns <mihails.strasuns@cloudfog.dev>
2023-12-02 18:22:01 +0100

    Replace Day01 task with the correct one
    Originally looked into year 2015 task instead by mistake :)

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,47 +1,53 @@
-use clap::Parser;
-use std::io::{stdin, Read};
+use onig::*;
+use std::io::stdin;
+#[macro_use]
+extern crate lazy_static;
 
-#[derive(Parser)]
-struct Args {
-    #[arg(short, long, action)]
-    entry: bool,
+fn str_to_num(s: &str, pos: usize) -> u32 {
+    if let Ok(num) = s[pos..pos + 1].parse::<u32>() {
+        return num;
+    }
+    let tail = &s[pos..];
+    if tail.starts_with("one") {
+        1
+    } else if tail.starts_with("two") {
+        2
+    } else if tail.starts_with("three") {
+        3
+    } else if tail.starts_with("four") {
+        4
+    } else if tail.starts_with("five") {
+        5
+    } else if tail.starts_with("six") {
+        6
+    } else if tail.starts_with("seven") {
+        7
+    } else if tail.starts_with("eight") {
+        8
+    } else if tail.starts_with("nine") {
+        9
+    } else {
+        unimplemented!();
+    }
 }
 
-#[derive(Debug, Default)]
-struct State {
-    sum: i64,
-    pos: i64,
-    basement_pos: Option<i64>,
+fn first_and_last(line: &str) -> (u32, u32) {
+    lazy_static! {
+        static ref RE_NUMISH: Regex =
+            Regex::new(r"(?=(\d|one|two|three|four|five|six|seven|eight|nine))").unwrap();
+    }
+    let nums: Vec<u32> = RE_NUMISH
+        .find_iter(line)
+        .map(|(a, _)| str_to_num(line, a))
+        .collect();
+    (nums[0], nums[nums.len()-1])
 }
 
 fn main() {
-    let args = Args::parse();
-    let input = stdin();
-
-    let result = input
-        .lock()
-        .bytes()
-        .map(|i| match i {
-            Ok(40) => 1,  // (
-            Ok(41) => -1, // )
-            _ => 0,
-        })
-        .fold(State::default(), |acc, i| State {
-            sum: acc.sum + i,
-            pos: acc.pos + 1,
-            basement_pos: if acc.basement_pos.is_none() && (acc.sum + i < 0) {
-                Some(acc.pos + 1)
-            } else {
-                acc.basement_pos
-            },
-        });
-
-    if args.entry {
-        println!(
-            "Entering basement at step: {}",
-            result.basement_pos.unwrap()
-        );
-    } else {
-        println!("Final position: {}", result.sum);
-    }
+    let result = stdin()
+        .lines()
+        .map(|s| first_and_last(&s.unwrap()))
+        .map(|(a, b)| a * 10 + b)
+        .fold(0, |acc, i| acc + i);
+    println!("{}", result);
 }
